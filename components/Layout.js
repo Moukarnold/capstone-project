ccb43677c8f04e3b7bafe47bca04972b4df65296
Arnold Moukam <moukarnold@gmail.com>
2023-05-25 10:26:37 +0200

    Rename and restructure Layout component

---
+++ components/Layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Tongue Twister",
+  description: "For improove your speaking skills",
+  keywords: "",
+};
+
+// Head component von Next.js
+
+export default function Layout({ children }) {
+  return <main>{children}</main>;
+}
