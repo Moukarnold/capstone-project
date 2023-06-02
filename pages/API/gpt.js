a02a621bd0c1715e4990df959833c645c6f64665
Arnold Moukam <moukarnold@gmail.com>
2023-06-02 12:16:47 +0200

    try to connect the api openai to the app.the openai key is already linked. what to check?  - essaie.js ,      gpt.js,    openaiConfig.js.

---
+++ pages/API/gpt.js
@@ -0,0 +1,9 @@
+import { createAnswer } from "@/utils/openaiConfig.js";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const answer = await createAnswer(req.body);
+    console.log("answer", answer);
+    return res.status(200).json(answer);
+  }
+}
