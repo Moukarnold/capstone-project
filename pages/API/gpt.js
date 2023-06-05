eb0ef8437d834b3df50b96b09f60da792ac8f01f
Arnold Moukam <moukarnold@gmail.com>
2023-06-05 09:28:03 +0200

    creation of a twister json file. the tongue twister are generate after choosing the options

--- pages/API/gpt.js
+++ pages/API/gpt.js
@@ -1,10 +1,1 @@
-/*import { createAnswer } from "@/utils/openaiConfig.js";
 
-export default async function handler(req, res) {
-  if (req.method === "POST") {
-    const answer = await createAnswer(req.body);
-    console.log("answer", answer);
-    return res.status(200).json(answer);
-  }
-}
-*/
